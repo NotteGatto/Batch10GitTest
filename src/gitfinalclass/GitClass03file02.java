e135b5bd02dfdc52b23994f43d1080872459705f
NotteGatto <ast2605@gmail.com>
2021-08-13 21:37:45 +0600

    the second commit of project

---
+++ src/gitfinalclass/GitClass03file02.java
@@ -0,0 +1,17 @@
+package gitfinalclass;
+
+public class GitClass03file02 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("this is the second commit file");
+
+		
+		
+		
+		
+		
+		
+	}
+
+}
